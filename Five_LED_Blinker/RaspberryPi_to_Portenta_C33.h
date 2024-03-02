df4ad67aa6bbdfbb9db484488f1311c1247c7b39
Dale Weber <hybotics@hybotics.dev>
2024-03-02 07:45:29 -0800

    Adding new Five LED Blinker code

---
+++ Five_LED_Blinker/RaspberryPi_to_Portenta_C33.h
@@ -0,0 +1,75 @@
+/*
+  Raspberry Pi to Arduino Portenta C33 Pin Mapping
+
+        Raspberry Pi          Portenta C33 / Aliases
+*/
+#define GPIO_02               29
+#define SDA1                  GPIO_02
+
+#define GPIO_03               40
+#define SCL1                  GPIO_03
+
+#define GPIO_04                0            //  RESETs MCU
+#define GPIO_GCLK             GPIO_04
+#define PWM0                  GPIO_04
+
+#define GPIO_05               93
+#define GPIO_06               33
+
+#define GPIO_07               12
+#define SPI_CE1_N             GPIO_07
+
+#define GPIO_08                2
+#define SPI_CE0_N             GPIO_08
+
+#define GPIO_09               45
+#define SPI_MISO              GPIO_09
+
+#define GPIO_10               46
+#define SPI_MOSI              GPIO_10
+
+#define GPIO_11               47
+#define SPI_CLK               GPIO_11
+
+#define GPIO_12               92
+#define GPIO_GEN1             GPIO_12
+
+#define GPIO_13                4
+
+#define GPIO_14               53
+#define TXD0                  GPIO_14
+
+#define GPIO_15               54
+#define RXD0                  GPIO_15
+
+#define GPIO_16                5
+
+#define GPIO_17                2
+#define GPIO_GEN0             GPIO_17
+#define PWM2                  GPIO_17
+
+#define GPIO_18               63
+#define GPIO_19               64
+#define GPIO_20               65
+#define GPIO_21               66
+
+#define GPIO_22               80
+#define GPIO_GEN3             GPIO_22
+
+#define GPIO_23               78
+#define GPIO_GEN4             GPIO_23
+
+#define GPIO_24               79
+#define GPIO_GEN5             GPIO_24
+
+#define GPIO_25                1
+#define GPIO_GEN6             GPIO_25
+#define PWM1                  GPIO_25
+
+#define GPIO_26                6
+
+#define GPIO_27               33
+#define GPIO_GEN2             GPIO_27
+
+#define SDA0                  11      //  I2C EEPROM
+#define SCL0                  12      //  I2C EEPROM
